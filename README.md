48283d9c2d457bb96619d907226117343607c131
megapixel23 <megapixel23@gmail.com>
2016-11-27 19:30:48 +0200

    BelongsToOneTrait added

---
+++ README.md
@@ -0,0 +1,11 @@
+Belongs To One Relation for Laravel 5
+=====================================
+
+Based on [Belongs To Many](https://laravel.com/docs/5.3/eloquent-relationships#defining-relationships) Relation. Returns one model instead of Collection of models.
+
+### Installation
+
+``` composer require megapixel23/belongs-to-one```
+
+### Usage
+[WorkInProgress]
---
+++ src/BelongsToOneTrait.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Megapixel23\Database\Eloquent\Relations;
+
+trait BelongsToOneTrait
+{
+    public function belongsToOne($related, $table = null, $foreignKey = null, $otherKey = null, $relation = null)
+    {
+        // If no relationship name was passed, we will pull backtraces to get the
+        // name of the calling function. We will use that function name as the
+        // title of this relation since that is a great convention to apply.
+        if (is_null($relation)) {
+            $relation = $this->getBelongsToManyCaller();
+        }
+
+        // First, we'll need to determine the foreign key and "other key" for the
+        // relationship. Once we have determined the keys we'll make the query
+        // instances as well as the relationship instances we need for this.
+        $foreignKey = $foreignKey ?: $this->getForeignKey();
+
+        $instance = new $related;
+
+        $otherKey = $otherKey ?: $instance->getForeignKey();
+
+        // If no table name was provided, we can guess it by concatenating the two
+        // models using underscores in alphabetical order. The two model names
+        // are transformed to snake case from their default CamelCase also.
+        if (is_null($table)) {
+            $table = $this->joiningTable($related);
+        }
+
+        // Now we're ready to create a new query builder for the related model and
+        // the relationship instances for the relation. The relations will set
+        // appropriate query constraint and entirely manages the hydrations.
+        $query = $instance->newQuery();
+
+        return new BelongsToOne($query, $this, $table, $foreignKey, $otherKey, $relation);
+    }
+}
